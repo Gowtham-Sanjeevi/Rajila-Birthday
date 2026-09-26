9e4bc56c4d77802ba05f213dc07423f6ff9ca57d
Gowtham-Sanjeevi <gowthamsanjeevi@outlook.com>
2026-09-26 13:04:09 +0530

    TIme issue fixed

--- home.js
+++ home.js
@@ -445,11 +445,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // CONFIGURATION
     // =========================================================
 
-<<<<<<< HEAD
-    const birthdayDate = new Date('September 26, 2026 13:10:00').getTime();
-=======
-    const birthdayDate = new Date('September 26, 2026 00:00:00').getTime();
->>>>>>> a3d347222609c53458d1f0d91377591d1fdc2ec4
+    const birthdayDate = new Date('September 26, 2026 13:30:00').getTime();
 
     const greetingText =
         "Hey Rajila! You're one of the most amazing people I've ever known! 💖";
