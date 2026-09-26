a42150431b49f0ad2f81aebec22ed00217cd454c
Gowtham-Sanjeevi <gowthamsanjeevi@outlook.com>
2026-09-26 19:20:09 +0530

    countdown changed

--- home.js
+++ home.js
@@ -445,7 +445,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // CONFIGURATION
     // =========================================================
 
-    const birthdayDate = new Date('September 26, 2026 19:16:00').getTime();
+    const birthdayDate = new Date('September 28, 2026 00:00:00').getTime();
 
     const greetingText =
         "Hey Rajila! You're one of the most amazing people I've ever known! 💖";
